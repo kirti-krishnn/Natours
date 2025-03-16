603882de14a6d511604bafd22c0fbafba3867d90
kirti-krishnn <kirti.krishnn@gmail.com>
2025-03-16 22:13:11 +0530

    Initial commit

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -17,4 +17,14 @@ class APIFeatures {
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
+
+  sort() {
+    if (this.queryString.sort) {
+      let sortStr = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortStr);
+      return this;
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+  }
 }
