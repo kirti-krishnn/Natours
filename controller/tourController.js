c80eb5a6e6fc61ffbf411409a0e899fbbb3f2fc5
kirti-krishnn <kirti.krishnn@gmail.com>
2025-04-09 15:00:26 +0530

    Initial commit

--- controller/tourController.js
+++ controller/tourController.js
@@ -87,3 +87,64 @@ exports.deleteTour = catchAsync(async (req, res) => {
     },
   });
 });
+
+exports.getStats = catchAsync(async (req, res) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+      },
+    },
+    { $sort: { avgPrice: 1 } },
+  ]);
+});
+
+exports.getMonthlyStats = catchAsync(async (req, res) => {
+  const MonthlyStats = await Tour.aggregate([
+    { $unwind: '$startDates' },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $monthly: '$startDates' },
+        numStartTours: { $sum: 1 },
+        tours: { $push: '$name' },
+      },
+    },
+    {
+      $addFields: { month: '$_id' },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+    {
+      $sort: { numTourStarts: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      MonthlyStats,
+    },
+  });
+});
