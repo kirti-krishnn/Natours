b8b52ec2e8921dc367455f006c8a539c85802e55
kirti-krishnn <kirti.krishnn@gmail.com>
2025-04-15 01:11:54 +0530

    Initial commit

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,11 @@
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.signUp= catchAsync(async(req,res,next)=>{
-    const user = await User.
+exports.signUp = catchAsync(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 });
